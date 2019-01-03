0bf4310ebece5c65a4d7a8389fdce4a034a004a0
Meghan Stang <mstang15@gmail.com>
2019-01-03 15:33:12 -0700

    adds users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController<ApplicationController
+
+  def create
+    user = User.create(user_params)
+    user[:api_key] = user.token
+    render json: UserSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+end
