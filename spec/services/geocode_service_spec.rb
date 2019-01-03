b23bd71b96fcc2673f4c5f02aaa3471d238198a5
Meghan Stang <mstang15@gmail.com>
2019-01-03 08:46:10 -0700

    adds vcr to geocode service

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe "Geocode Service", type: :service do
   it "returns coords for location" do
 
-    location = "denver,co"
-    service = GeocodeService.new(location)
+    VCR.use_cassette('geocode_request') do
+      location = "denver,co"
+      @service = GeocodeService.new(location)
+      results = @service.coords
 
-    results = service.coords
-
-    expect(results).to eq({:lat=>39.7392358, :lng=>-104.990251})
+      expect(results).to eq({:lat=>39.7392358, :lng=>-104.990251})
+    end
   end
 end
