7a552c353d69f1b4f1fba9f6dec8f754f2466952
Meghan Stang <mstang15@gmail.com>
2019-01-03 08:45:56 -0700

    adds vcr to darksky service

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 describe "Darksky Service", type: :service do
-  it "returns coords for location" do
 
-    city = "denver,co"
+  before(:each) do
+    VCR.use_cassette('dark_sky_request') do
+      city = "denver,co"
 
-    location = GeocodeService.new(city).coords
-    service = DarkskyService.new(location[:lat],location[:lng])
+      @location = {:lat=>39.7392358, :lng=>-104.990251}
+      @service = DarkskyService.new(@location[:lat],@location[:lng])
 
-    results = service.forecast_data
-
-    expect(results[:latitude]).to eq(location[:lat])
-    expect(results[:longitude]).to eq(location[:lng])
+      @results = @service.forecast_data
+    end
   end
+
+  it "returns coords for location" do
+    expect(@results[:latitude]).to eq(@location[:lat])
+    expect(@results[:longitude]).to eq(@location[:lng])
+  end
+
+      # it "is used to create api for our app" do
+      #   visit '/api/v1/forecast?location=denver,co'
+      #
+      #   expect(response).to be_successfull
+      # end
 end
