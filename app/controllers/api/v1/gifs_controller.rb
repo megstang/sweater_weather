a990c573a7a551ef0099f99e31a77c0bfbfef593
Meghan Stang <mstang15@gmail.com>
2019-01-04 10:01:47 -0700

    fixes gifs controller to call to write method

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::GifsController<ApplicationController
 
   def index
-    render json: GiphySerializer.new(Forecast.new(params[:location]))
+    giphy_objects = Forecast.new(params[:location]).giphy_service_objects
+    render json: GiphySerializer.new(giphy_objects)
   end
 
 end
