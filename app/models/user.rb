a541151609bceae883873f75e7897779e51eedfb
Meghan Stang <mstang15@gmail.com>
2019-01-05 13:30:08 -0700

    moves token method to controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User <ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
-  
-  def token
-    SecureRandom.urlsafe_base64.to_s
-  end
+
 
 end
