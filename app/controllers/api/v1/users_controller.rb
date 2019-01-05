cf2f577cd880070b65a2dbffcd4d8131d9df5a04
Meghan Stang <mstang15@gmail.com>
2019-01-05 14:38:35 -0700

    removes attribute readers for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::UsersController<ApplicationController
 
   def create
-    @user = User.create(all_params) if matching_passwords?
-    if @user.save
-      session[:user_id] = @user.id
-      render json: UserSerializer.new(@user)
+    user = User.create(all_params) if matching_passwords?
+    if user.save
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
     end
   end
 
