65c722f46ce841f7677290392e49a36404f4acab
Meghan Stang <mstang15@gmail.com>
2019-01-05 14:38:51 -0700

    adds relationship to favorites for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User <ApplicationRecord
   has_secure_password
+  has_many :favorites
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
+  validates_presence_of :api_key, on: :create
 
 
 end
